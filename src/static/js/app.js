bd2534795c7d3ebeeccad803897ea5322f8bd383
EigoTaya <eigo-ta@mm-dhcp-124-037.naist.jp>
2020-12-24 14:46:21 +0900

    fix

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,6 +3,9 @@ function App() {
     return (
         <Container>
             <Row>
+                <h1>Todo App</h1>
+            </Row>
+            <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
                 </Col>
